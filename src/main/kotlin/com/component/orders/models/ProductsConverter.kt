80a0fcb9a371466490a60ef6b3943ad6f3303243
Hari Krishnan <harikrishnan83@gmail.com>
2025-03-17 14:41:31 +0530

    Checking in Gradle and Specmatic Kafka JAR, handling the double encoding issue in JSON array

--- src/main/kotlin/com/component/orders/models/ProductsConverter.kt
+++ src/main/kotlin/com/component/orders/models/ProductsConverter.kt
@@ -1,5 +1,6 @@
 package com.component.orders.models
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import jakarta.persistence.AttributeConverter
@@ -17,9 +18,9 @@ class ProductsConverter : AttributeConverter<List<Product>, String> {
         return dbData?.let {
             try {
                 objectMapper.readValue<List<Product>>(it)
-            } catch (e: Throwable) {
-                println("Error deserializing JSON $dbData to List<Product>: ${e.message}")
-                emptyList()
+            } catch (_: MismatchedInputException) {
+                val jsonWithoutDoubleEncoding = objectMapper.readValue<String>(it)
+                objectMapper.readValue<List<Product>>(jsonWithoutDoubleEncoding)
             }
         } ?: emptyList()
     }
