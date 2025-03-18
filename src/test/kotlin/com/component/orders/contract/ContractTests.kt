7198d0785ec3c2f83e8252b1a8d3a6e06d74658c
Hari Krishnan <harikrishnan83@gmail.com>
2025-03-18 06:07:52 +0530

    asserting order status in DB

--- src/test/kotlin/com/component/orders/contract/ContractTests.kt
+++ src/test/kotlin/com/component/orders/contract/ContractTests.kt
@@ -1,5 +1,8 @@
 package com.component.orders.contract
 
+import com.component.orders.dal.OrderRepository
+import com.component.orders.models.Order
+import com.component.orders.models.OrderStatus
 import io.specmatic.kafka.CONSUMER_GROUP_ID
 import io.specmatic.kafka.Expectation
 import io.specmatic.kafka.KafkaMock
@@ -7,13 +10,18 @@ import io.specmatic.test.SpecmaticContractTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestContextManager
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ContractTests : SpecmaticContractTest {
+    @Autowired
+    private lateinit var orderRepository: OrderRepository
 
     companion object {
         private var kafkaMock: KafkaMock? = null
+        private lateinit var contractTestInstance: ContractTests
         private const val APPLICATION_HOST = "localhost"
         private const val APPLICATION_PORT = "8080"
         private const val KAFKA_MOCK_HOST = "localhost"
@@ -31,17 +39,27 @@ class ContractTests : SpecmaticContractTest {
             System.setProperty("filter", "PATH!='/health'")
             System.setProperty(CONSUMER_GROUP_ID, CONSUMER_GROUP_IDENTIFIER)
 
+            val testContextManager = TestContextManager(ContractTests::class.java)
+            testContextManager.prepareTestInstance(contractTestInstance)
+
             // Start Specmatic Kafka Mock and set the expectations
             kafkaMock = KafkaMock.startInMemoryBroker(KAFKA_MOCK_HOST, KAFKA_MOCK_PORT)
             kafkaMock?.setExpectations(listOf(Expectation("create-order-request", EXPECTED_NUMBER_OF_MESSAGES)))
+
+            contractTestInstance = ContractTests()
         }
 
         @JvmStatic
         @AfterAll
         fun tearDown() {
-
+            val orderId = 3
+            assertThat( getOrderFromDb(orderId).status ).isEqualTo(OrderStatus.Accepted)
             val result = kafkaMock!!.stop()
             assertThat(result.success).withFailMessage(result.errors.joinToString()).isTrue
         }
+
+        private fun getOrderFromDb(orderId: Int): Order =
+            contractTestInstance.orderRepository.findById(orderId)
+                .orElseThrow { RuntimeException("Order with id $orderId not found") }
     }
 }
